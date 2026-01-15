7ae231ea6456c1e3e14790b6f8ca131d79560f81
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-01-15 12:43:08 +0530

    day8 sum two solution added

---
+++ src/main/java/org/hklearning/leetcode150/sumtwoII167/Attempt1.java
@@ -0,0 +1,21 @@
+package org.hklearning.leetcode150.sumtwoII167;
+
+public class Attempt1 {
+    public int[] twoSum(int[] numbers, int target) {
+        int i=0, j=numbers.length-1;
+        int[] result=new int[2];
+        while(i<=j){
+            int sum = numbers[i]+numbers[j];
+            if(sum>target)
+                j--;
+            else if(sum<target)
+                i++;
+            else{
+                result[0]=i+1;
+                result[1]=j+1;
+                break;
+            }
+        }
+        return result;
+    }
+}
