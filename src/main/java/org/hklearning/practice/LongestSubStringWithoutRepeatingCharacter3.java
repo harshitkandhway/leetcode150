1ada390339efec5a6c01c1ba15346bc1efd52c38
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-01-23 03:25:28 +0530

    Q2-practice added

---
+++ src/main/java/org/hklearning/practice/LongestSubStringWithoutRepeatingCharacter3.java
@@ -0,0 +1,26 @@
+package org.hklearning.practice;
+
+public class LongestSubStringWithoutRepeatingCharacter3 {
+    public int lengthOfLongestSubstring(String s) {
+        int i=0,j=0,max=0;
+        boolean[] arr = new boolean[128];
+        while(j<s.length()){
+            char c = s.charAt(j);
+            if(!arr[c]){
+                arr[c]=true;
+            } else{
+                max = Math.max(max, j-i);
+                if(c!=s.charAt(i)){
+                    while(s.charAt(i)!=c){
+                        arr[s.charAt(i)]=false;
+                        i++;
+                    }
+                    arr[c] = true;
+                }
+                i++;
+            }
+            j++;
+        }
+        return Math.max(max, j-i);
+    }
+}
