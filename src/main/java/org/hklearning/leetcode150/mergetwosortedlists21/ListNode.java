2f348ed2d3eef0afb551b48fbb71fb125c90c9fc
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-01-29 19:57:34 +0530

    day22 code added

---
+++ src/main/java/org/hklearning/leetcode150/mergetwosortedlists21/ListNode.java
@@ -0,0 +1,9 @@
+package org.hklearning.leetcode150.mergetwosortedlists21;
+
+public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
