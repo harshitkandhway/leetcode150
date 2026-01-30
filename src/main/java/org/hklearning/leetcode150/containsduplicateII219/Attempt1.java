b63d8ce886db4c25cfba9ccd354c469a9cf03118
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-01-31 00:10:18 +0530

    day 23 code added

---
+++ src/main/java/org/hklearning/leetcode150/containsduplicateII219/Attempt1.java
@@ -0,0 +1,20 @@
+package org.hklearning.leetcode150.containsduplicateII219;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Attempt1 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> window = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (window.contains(nums[i])) {
+                return true;
+            }
+            window.add(nums[i]);
+            if (window.size() > k) {
+                window.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
