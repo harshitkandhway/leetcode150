310ceafec715469e1050536435bb400de0412258
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-02-03 00:09:57 +0530

    day 26 code added

---
+++ src/main/java/org/hklearning/leetcode150/removenthnodefromend19/Attempt1.java
@@ -0,0 +1,19 @@
+package org.hklearning.leetcode150.removenthnodefromend19;
+
+public class Attempt1 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode first = dummy;
+        ListNode second = dummy;
+        for (int i = 0; i <= n; i++) {
+            first = first.next;
+        }
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
