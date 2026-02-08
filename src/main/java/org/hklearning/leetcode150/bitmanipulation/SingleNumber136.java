994139bb963dd99b79c38dde3991274b7e8f02bb
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-02-08 23:14:17 +0530

    day 31-q1 added

---
+++ src/main/java/org/hklearning/leetcode150/bitmanipulation/SingleNumber136.java
@@ -0,0 +1,10 @@
+package org.hklearning.leetcode150.bitmanipulation;
+
+public class SingleNumber136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for(int num : nums)
+            result ^= num;
+        return result;
+    }
+}
