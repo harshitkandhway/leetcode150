ab3729510433faf1a312e689e8cf6acb4865a6d3
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-02-15 16:50:34 +0530

    day - 38 q1 added

---
+++ src/main/java/org/hklearning/leetcode150/tree/CountCompleteTreeNodes222.java
@@ -0,0 +1,13 @@
+package org.hklearning.leetcode150.tree;
+
+public class CountCompleteTreeNodes222 {
+    int count=0;
+    public int countNodes(TreeNode root) {
+        if(root!=null){
+            count++;
+            countNodes(root.left);
+            countNodes(root.right);
+        }
+        return count;
+    }
+}
