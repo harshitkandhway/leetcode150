82b199063c54242d48fc31333baae4c48862d5ed
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-02-22 19:08:58 +0530

    day 45, q1 added

---
+++ src/main/java/org/hklearning/leetcode150/bitmanipulation/SingleNumberII137.java
@@ -0,0 +1,17 @@
+package org.hklearning.leetcode150.bitmanipulation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SingleNumberII137 {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) return entry.getKey();
+        }
+        return -1;
+    }
+}
