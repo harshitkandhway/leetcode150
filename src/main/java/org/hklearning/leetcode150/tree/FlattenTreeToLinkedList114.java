72d65bc09f628690c640b40f95c45c965dc312d4
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-02-26 09:28:46 +0530

    day 49- q1 added

---
+++ src/main/java/org/hklearning/leetcode150/tree/FlattenTreeToLinkedList114.java
@@ -0,0 +1,16 @@
+package org.hklearning.leetcode150.tree;
+
+public class FlattenTreeToLinkedList114 {
+    private TreeNode prev = null;
+
+    public void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        flatten(root.right);
+        flatten(root.left);
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
