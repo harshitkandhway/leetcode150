e042b6b78e483f8770005095c6b69b0b07472a90
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-03-12 23:42:05 +0530

    day 64 q1 added

---
+++ src/main/java/org/hklearning/leetcode150/searchinsert35/Attempt1.java
@@ -0,0 +1,20 @@
+package org.hklearning.leetcode150.searchinsert35;
+
+public class Attempt1 {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
