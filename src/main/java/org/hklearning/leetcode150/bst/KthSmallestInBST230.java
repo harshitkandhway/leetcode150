f4e4651eabccd0570028bd042982f5d7ba3bf5ed
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-03-18 11:21:20 +0530

    day 70 q1 added

---
+++ src/main/java/org/hklearning/leetcode150/bst/KthSmallestInBST230.java
@@ -0,0 +1,27 @@
+package org.hklearning.leetcode150.bst;
+
+import org.hklearning.TreeNode;
+
+public class KthSmallestInBST230 {
+    private int count = 0;
+    private int result = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return result;
+    }
+
+    private void inorder(TreeNode node, int k) {
+        if (node == null) return;
+
+        inorder(node.left, k);
+
+        count++;
+        if (count == k) {
+            result = node.val;
+            return;
+        }
+
+        inorder(node.right, k);
+    }
+}
