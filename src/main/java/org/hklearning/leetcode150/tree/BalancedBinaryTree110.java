102ea1418109194bfa4affb22b467c86758e04cd
Harshit Kandhway <kandhway.harshit198@gmail.com>
2026-04-14 20:18:35 +0530

    day 93 q1 added

---
+++ src/main/java/org/hklearning/leetcode150/tree/BalancedBinaryTree110.java
@@ -0,0 +1,21 @@
+package org.hklearning.leetcode150.tree;
+
+public class BalancedBinaryTree110 {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(node.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = getHeight(node.right);
+        if (rightHeight == -1) return -1;
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+}
